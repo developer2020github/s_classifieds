c1a916a58e791a6ae1a0ce2da5a1fb246c3f4e70
developer2020 <dev276236@gmail.com>
2017-01-06 21:25:27 +0400

    cleaned up test_database module

--- s_catalog/test_database.py
+++ s_catalog/test_database.py
@@ -1,3 +1,9 @@
+"""
+This module contains various test and helper functions.
+It does not have to be in production version of the application.
+Function names are self-explanatory.
+"""
+
 import create_database
 import database
 import json
@@ -22,36 +28,6 @@ def test_get_ads_to_display():
 
 def test_categories_with_subcategories():
     print json.dumps(database.get_categories_with_subcategories())
-
-
-def test_one():
-    print "test_one"
-    ads_html = list()
-    ads, total_number_of_ads, min_ad_idx_displayed, max_ad_idx_displayed = \
-        database.get_ads_to_display(city_id=1,
-                                    min_idx=0,
-                                    number_of_records_to_include=10,
-                                    sub_category_id=9,
-                                    created_within_days=-1,
-                                    sort_by="", debug_print=True)
-    print total_number_of_ads, min_ad_idx_displayed, max_ad_idx_displayed
-    print ads
-    if total_number_of_ads>0:
-        for ad in ads:
-            ads_html.append(database.ad_to_dict(ad))
-#test_#test_categories_with_subcategories()
-
-
-def test_empty_query():
-    ads =  database.session.query(create_database.Ad).filter(create_database.Ad.user_id == 1234546)
-    print ads.all()
-    print ads.count()
-
-
-def test_query():
-    ads =  database.session.query(create_database.Ad).filter(create_database.Ad.user_id == 9)
-    print ads.all()
-    print ads.count()
 
 
 def test_filtering_by_user():
@@ -106,13 +82,10 @@ def test_random_category_and_subcategory():
     category = database.session.query(create_database.Category).filter_by(id=sub_category.category_id).first()
     print category.name
 
+
 def test_get_ad_by_id():
     for i in range(1, 10):
         database.print_ad (database.get_ad_by_id(i))
-
-
-'''
-'''
 
 
 def test_get_user_id_by_email():
